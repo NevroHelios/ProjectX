e24a3efe2ea24961631fb5712f0de7771ad3c373
ishant9805 <ishant9805@gmail.com>
2023-10-01 15:57:55 +0530

    Started app

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+
+# Create app instance of Flask
+app = Flask(__name__)
+
+
+# Create a route for the home page
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+# run the app
+if __name__ == "__main__":
+    app.run(debug=True)
