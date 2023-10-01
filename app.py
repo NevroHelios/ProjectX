c5560741bf7460e4bdce69d8c4ec2c4c439c96d4
ishant9805 <ishant9805@gmail.com>
2023-10-01 17:11:15 +0530

    Added command to run the app

--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 
+# Command to start the server
+# flask run --debug --port 9420
+
 # Create app instance of Flask
 app = Flask(__name__)
-
 
 # Create a route for the home page
 @app.route('/')
 def home():
     return render_template('index.html')
-
-
-# run the app
-if __name__ == "__main__":
-    app.run(debug=True)
