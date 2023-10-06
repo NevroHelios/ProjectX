6acddbddd849dce855d1f5873ce8da9889295240
NevroHelios <dasharka05@gmail.com>
2023-10-06 23:27:37 +0530

    A little Update

--- app.py
+++ app.py
@@ -10,6 +10,12 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/')
+def greet():
+    return render_template('greet.html')
+
+
 # run the app
 if __name__ == "__main__":
     app.run(debug=True)
+
