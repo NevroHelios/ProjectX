562699c2105d5da391761316041fcb6d4c838562
Nimish Shinde <106652972+whynimoisalive@users.noreply.github.com>
2023-10-12 22:49:49 +0530

    Adding Description for Pull Request - Template Changes
     I've made to the project. I have modified the templates and created new HTML, CSS, and JavaScript files to enhance the project's functionality and appearance.
    This pull request is intended to introduce these changes and improvements to the project. I believe these updates will make a positive impact. You can find the details of the changes in the pull request description.Your feedback and comments on these changes would be greatly appreciated. Please review the pull request and let me know if there are any specific aspects you'd like me to address or if you have any questions about the modifications.

---
+++ sliding-images-master/index.js
@@ -0,0 +1,100 @@
+const track = document.getElementById("image-track");
+const slideFriction = 1;
+
+let scrollValue = 0;
+const modalContainer = document.getElementById("modal-container");
+const modalContent = document.getElementById("modal-content");
+const modalImage = document.getElementById("modal-image");
+const closeModal = document.getElementById("close-modal");
+
+for (const image of track.getElementsByClassName("image")) {
+    image.addEventListener("click", () => {
+        modalImage.src = image.src;
+        modalContainer.style.display = "block";
+     
+    });
+}
+
+closeModal.addEventListener("click", () => {
+    modalContainer.style.display = "none";
+});
+
+
+window.addEventListener("wheel", (e) => {
+    scrollValue += e.deltaY;
+    const maxDelta = window.innerWidth * slideFriction;
+
+    const percentage = (scrollValue / maxDelta) * 90;
+    const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
+    const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -90);
+
+    track.dataset.percentage = nextPercentage;
+
+    track.animate(
+        {
+            transform: `translate(${nextPercentage}%, -50%)`
+        },
+        {
+            duration: 1200,
+            fill: "forwards"
+        }
+    );
+
+    for (const image of track.getElementsByClassName("image")) {
+        image.animate(
+            {
+                objectPosition: `${nextPercentage + 100}% 50%`
+            },
+            {
+                duration: 1200,
+                fill: "forwards"
+            }
+        );
+    }
+});
+
+window.onmousedown = (e) => {
+    track.dataset.mouseDownAt = e.clientX;
+};
+
+window.onmousemove = (e) => {
+    if (track.dataset.mouseDownAt === "0") return;
+
+    const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX;
+    const maxDelta = window.innerWidth * slideFriction;
+
+    const percentage = -(mouseDelta / maxDelta) * 100;
+    const nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
+    const nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+    track.dataset.percentage = nextPercentage;
+
+    track.animate(
+        {
+            transform: `translate(${nextPercentage}%, -50%)`
+        },
+        {
+            duration: 1200,
+            fill: "forwards"
+        }
+    );
+
+    for (const image of track.getElementsByClassName("image")) {
+        image.animate(
+            {
+                objectPosition: `${nextPercentage + 100}% 50%`
+            },
+            {
+                duration: 1200,
+                fill: "forwards"
+            }
+        );
+    }
+};
+
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = 0;
+    track.dataset.prevPercentage = track.dataset.percentage;
+
+    
+};
